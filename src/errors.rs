1e0ef16d5518dc28c835c72ec6628d9cdf012e93
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2023-09-02 10:18:29 -0500

    clippy

--- src/errors.rs
+++ src/errors.rs
@@ -26,13 +26,13 @@ impl Error for BLSError {}
 impl fmt::Display for BLSError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            BLSError::BadInfinityEncoding(i) => write!(f, "Bad encoding: infinity bit set but found non-zero bits in byte at index {}", i),
+            BLSError::BadInfinityEncoding(i) => write!(f, "Bad encoding: infinity bit set but found non-zero bits in byte at index {i}"),
             BLSError::DeserializationError(ref err) => err.fmt(f),
-            BLSError::IntegerTooLarge(ref n, l) => write!(f, "Integer too large: cannot fit {} into a byte string of length {}", n, l),
+            BLSError::IntegerTooLarge(ref n, l) => write!(f, "Integer too large: cannot fit {n} into a byte string of length {l}"),
             BLSError::NoSignaturesToAggregate => write!(f, "Cannot aggregate signatures: no signatures were passed in"),
             BLSError::SignatureNotInCorrectSubgroup => write!(f, "Signature point is not in the correct subgroup. Please check the passed in secret key value."),
-            BLSError::WrongSizeForSignature(l) => write!(f, "Signature bytes must have length 96 or 192. Got {}", l),
-            BLSError::WrongSizeForPublicKey(l) => write!(f, "Public key bytes must have length 48 or 96. Got {}", l),
+            BLSError::WrongSizeForSignature(l) => write!(f, "Signature bytes must have length 96 or 192. Got {l}"),
+            BLSError::WrongSizeForPublicKey(l) => write!(f, "Public key bytes must have length 48 or 96. Got {l}"),
         }
     }
 }
